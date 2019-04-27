6d7ecf91591498919d999d7b31ea16836f943676
TintifaxTheGreat <eugen.lindorfer@lindy.net>
2019-04-27 12:31:23 +0200

    add more goals

--- deal/goal.go
+++ deal/goal.go
@@ -4,7 +4,8 @@ import "math/bits"
 
 type goal func(players AllPlayers) [PLAYERS] int
 
-var GoalHearts = goal(func(players AllPlayers) [PLAYERS] int {
+// avoid hearts in the tricks
+var GoalNoHearts = goal(func(players AllPlayers) [PLAYERS] int {
 	var points [PLAYERS] int
 	for player := uint(0); player < PLAYERS; player++ {
 		// count all hearts in the player's tricks
@@ -12,3 +13,35 @@ var GoalHearts = goal(func(players AllPlayers) [PLAYERS] int {
 	}
 	return points
 })
+
+// avoid making tricks
+var GoalNoTricks = goal(func(players AllPlayers) [PLAYERS] int {
+	var points [PLAYERS] int
+	for player := uint(0); player < PLAYERS; player++ {
+		// count all the players tricks
+		points[player] -= bits.OnesCount64(uint64(*players[player].Card().tricks)) / 4
+	}
+	return points
+})
+
+// avoid queens in the tricks
+var GoalNoQueens = goal(func(players AllPlayers) [PLAYERS] int {
+	var points [PLAYERS] int
+	for player := uint(0); player < PLAYERS; player++ {
+		// count all queens in the player's tricks
+		points[player] -= 2 * bits.OnesCount64(uint64(ALLFIGURES[2] & *players[player].Card().tricks))
+	}
+	return points
+})
+
+/*
+// avoid the king of hearts, and making the last trick
+var GoalNoGepetto = goal(func(players AllPlayers) [PLAYERS] int {
+	var points [PLAYERS] int
+	for player := uint(0); player < PLAYERS; player++ {
+		// count all hearts in the player's tricks
+		points[player] -= bits.OnesCount64(uint64(ALLCOLORS[0] & *players[player].Card().tricks))
+	}
+	return points
+})
+*/
